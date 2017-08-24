44dbb221276802cbbc11288c426962435779197e
vardaro <anthonyavardaro@yahoo.com>
2017-08-24 18:30:59 -0500

    oops

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const fs = require('fs');
 const { env } = process;
 const sub = 'circlejerk';
 
-process.env.clientId = 'c74Q1dKouiMGNA'
-process.env.clientSecret = 'M2UJaQvXhctXqC_7ItNLOxTpyCQ'
-process.env.refreshToken = '33133423-kXmv7O67gfM0Ce8gp6jIbjV6Hk4'
-process.env.userAgent = '9A25PnHxZY7nWQ'
-process.env.consumer_key = 'CPbb2wjGYuZO6IIYlVHbnbF27'
-process.env.consumer_secret = 'usDXlTFf1ssGqrYDiitv8hJyp6MyBHaVza3l2A2bY6laMOgwpw'
-process.env.access_token_key = '898926970427375616-XPeUpKzTjLLYj6bVwcOTNlbAXdQ4i4T'
-process.env.access_token_secret = 'G8e7DSdzFsextIpmALawhXIiBNMWwGQBIj4gCsurRkjGm'
-
 const r = new snoowrap({
     clientId: env.clientId,
     clientSecret: env.clientSecret,
